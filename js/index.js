075c188d32302b1098224caa676d459639d0485b
Lucas Fernandez Nicolau <lucasfnicolau@protonmail.com>
2021-12-09 03:51:41 -0300

    Improving performance.

--- js/index.js
+++ js/index.js
@@ -75,35 +75,63 @@ function setup() {
   deviceContainer.style.display = 'flex';
   deviceContainer.style.justifyContent = 'center';
 
-  const device = document.getElementById('device');
   const deviceHeight = window.innerHeight / 1.2;
   const deviceWidth = deviceHeight * 0.501752746;
-  device.style.display = 'flex';
-  device.style.justifyContent = 'center';
-  device.style.width = `${deviceWidth}px`;
-  device.style.height = `${deviceHeight}px`;
-  device.style.background = `${platform === 'iOS' ? 'url(./assets/img/iPhone_Bezel.png)' : 'url(./assets/img/android_Bezel.png)' }`;
-  device.style.backgroundRepeat = 'no-repeat';
-  device.style.backgroundSize = 'contain';
-  device.style.backgroundPosition = 'center';
 
-  const appIcons = (platform === 'iOS' ? iOSApps : androidApps);
-  const apps = document.getElementById('apps');
-  apps.style.display = 'grid';
-  apps.style.width = `${deviceWidth}px`;
-  apps.style.marginTop = `${deviceHeight / 8}px`;
-  apps.style.marginLeft = '50px';
-  apps.style.marginRight = '50px';
-  apps.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
-  apps.style.gridTemplateRows = `repeat(${appIcons.length}, 1fr)`;
-  apps.style.gridColumn = `${deviceHeight / 14}px`;
-  apps.innerHTML = '';
-  for (let i = 0; i < appIcons.length; i++) {
-    apps.appendChild(appIcon(appIcons[i].name, deviceHeight));
+  const iOSDevice = document.getElementById('ios-device');
+  iOSDevice.style.display = platform === 'iOS' ? 'flex' : 'none';
+  iOSDevice.style.justifyContent = 'center';
+  iOSDevice.style.width = `${deviceWidth}px`;
+  iOSDevice.style.height = `${deviceHeight}px`;
+  iOSDevice.style.background = 'url(./assets/img/iPhone_Bezel.png)';
+  iOSDevice.style.backgroundRepeat = 'no-repeat';
+  iOSDevice.style.backgroundSize = 'contain';
+  iOSDevice.style.backgroundPosition = 'center';
+
+  const iOSAppsGrid = document.getElementById('ios-apps');
+  iOSAppsGrid.style.display = 'grid';
+  iOSAppsGrid.style.width = `${deviceWidth}px`;
+  iOSAppsGrid.style.marginTop = `${deviceHeight / 8}px`;
+  iOSAppsGrid.style.marginLeft = '50px';
+  iOSAppsGrid.style.marginRight = '50px';
+  iOSAppsGrid.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
+  iOSAppsGrid.style.gridTemplateRows = `repeat(${iOSApps.length}, 1fr)`;
+  iOSAppsGrid.style.gridColumn = `${deviceHeight / 14}px`;
+  iOSAppsGrid.style.visibility = platform === 'iOS' ? 'visible' : 'hidden';
+  iOSAppsGrid.innerHTML = '';
+  for (let i = 0; i < iOSApps.length; i++) {
+    iOSAppsGrid.appendChild(appIcon(iOSApps[i].name, deviceHeight, 'iOS'));
   }
+
+  const androidDevice = document.getElementById('android-device');
+  androidDevice.style.display = platform === 'Android' ? 'flex' : 'none';
+  androidDevice.style.justifyContent = 'center';
+  androidDevice.style.width = `${deviceWidth}px`;
+  androidDevice.style.height = `${deviceHeight}px`;
+  androidDevice.style.background = 'url(./assets/img/Android_Bezel.png)';
+  androidDevice.style.backgroundRepeat = 'no-repeat';
+  androidDevice.style.backgroundSize = 'contain';
+  androidDevice.style.backgroundPosition = 'center';
+
+  const androidAppsGrid = document.getElementById('android-apps');
+  androidAppsGrid.style.display = 'grid';
+  androidAppsGrid.style.width = `${deviceWidth}px`;
+  androidAppsGrid.style.marginTop = `${deviceHeight / 8}px`;
+  androidAppsGrid.style.marginLeft = '50px';
+  androidAppsGrid.style.marginRight = '50px';
+  androidAppsGrid.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
+  androidAppsGrid.style.gridTemplateRows = `repeat(${androidApps.length}, 1fr)`;
+  androidAppsGrid.style.gridColumn = `${deviceHeight / 14}px`;
+  androidAppsGrid.style.visibility = platform === 'Android' ? 'visible' : 'hidden';
+  androidAppsGrid.innerHTML = '';
+  for (let i = 0; i < androidApps.length; i++) {
+    androidAppsGrid.appendChild(appIcon(androidApps[i].name, deviceHeight, 'Android'));
+  }
+
 
   const platformSwitch = document.getElementById('platform-switch');
   platformSwitch.style.width = `${deviceWidth / 5.5}px`;
+  platformSwitch.style.minWidth = '70px';
   platformSwitch.style.cursor = 'pointer';
   platformSwitch.onclick = switchPlatform;
   platformSwitch.src = platform === 'iOS' ? './assets/img/switch_ios.svg' : './assets/img/switch_android.svg';
@@ -115,7 +143,7 @@ function switchPlatform() {
   setup();
 }
 
-function appIcon(name, deviceHeight) {
+function appIcon(name, deviceHeight, platform) {
   const div = document.createElement('div');
   const img = document.createElement('img');
   const span = document.createElement('span');
@@ -136,7 +164,7 @@ function appIcon(name, deviceHeight) {
 
   span.innerText = name;
   span.style.textAlign = 'center';
-  span.style.fontSize = `${window.innerHeight / 100}px`;
+  span.style.fontSize = `${deviceHeight / 70}px`;
   span.style.color = '#fff';
 
   div.appendChild(img);
